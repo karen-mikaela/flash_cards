374ab19333a00530f73675c6eb965cbcc098bed0
karen-mikaela <karen.mikaela@gmail.com>
2013-12-10 23:57:23 -0200

    add, remove and reset card

--- app/controller/cardController.js
+++ app/controller/cardController.js
@@ -3,14 +3,14 @@
     var cardController = function ($scope, cardFactory, categoryFactory) {
         $scope.cards = [];
         $scope.categories = [];
-        $scope.isCollapsed = false;
+        $scope.isCollapsed = true;
         init()
 
-        $scope.refreshCards = function () {
+        $scope.refreshCards = function(){
             init();
         }
 
-        function init() {
+        function init(){
             $scope.cards = cardFactory.getCards();
             $scope.categories = categoryFactory.getCategories();
         }
@@ -18,8 +18,29 @@
         $scope.collapse = function(){
             $scope.isCollapsed = !$scope.isCollapsed;
         }
+
         $scope.close = function(){
             $scope.isCollapsed = true;
+        }
+
+        $scope.add = function(){
+            $scope.cards.push({"front": $scope.card.front,"back":$scope.card.back, "category":$scope.card.category});
+            reset();
+        }
+
+        $scope.remove = function(){
+            console.log(this.card);            
+            var index = $scope.cards.indexOf(this.card);
+            console.log("vai eliminar "+ ($scope.cards[index]).front);
+            console.log("vai eliminar de id  "+ index);
+            $scope.cards.splice(index, 1); 
+
+        }
+
+        function reset(){
+            $scope.card.front = "";
+            $scope.card.back = "";
+            $scope.card.category ="";
         }
 
     };
